1036c1eee09a4e2b68fa6399939c2a6f35528356
Sneh Shrivastava <subham4shrivastava@gmail.com>
2020-08-06 15:55:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-import React,{Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App'; //to use default app exported form app.js
+import './index.css';
 
 ReactDOM.render(<App></App>,document.getElementById('root'));
+/*
+new Promise((resolve,reject)=>{
+    return reject(new Error('No Beers'));
+
+    setTimeout(()=>{
+        console.log('Bears');
+        resolve();
+    },2000);
+})
+.then(()=>{
+    console.log('Beats');
+    console.log('Battlestart Galactica');
+})
+.catch(error=> console.log('error',error));*/
