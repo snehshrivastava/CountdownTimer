2a7ffb7355b140e2ba748dfab93c5cb59543b4ca
snehshrivastava <subham4shrivastava@gmail.com>
2020-08-09 16:18:14 +0530

    added css

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 
 class App extends Component{
-    state = {h:0,m:0,s:0,seconds:0,timer:false,interval:0,stopon:false,starton:false,submiton:true};
+    state = {h:0,m:0,s:0,seconds:0,timer:false,interval:0,stopon:false,starton:true,submiton:true};
     convertTime=(secs)=>{
 //        console.log('convert time',secs);
         const hours = Math.floor(secs/(60*60));
@@ -14,10 +14,13 @@ class App extends Component{
     }
 
     startTimer=()=>{
-        this.convertTime(this.state.seconds);
+        const secs = Number(this.state.h*60*60)+Number(this.state.m*60)+Number(this.state.s);
+        this.convertTime(secs);
+        this.setState({})
+        
         //this.setState({time:t});
       //  this.setState({timer:false});
-        console.log('start timer',this.state.timer);
+      //  console.log('start timer',this.state.timer);
       if(!this.state.timer && !this.state.interval)//for not calling the 
         this.state.interval=setInterval(this.countDown,1000);
     }
@@ -41,18 +44,36 @@ class App extends Component{
 
     changeTimer=()=>{
         this.setState({timer:false,stopon:true,starton:false,submiton:false});// onchange event
-        console.log('change timer',this.state.timer);
+        //console.log('change timer',this.state.timer);
         this.startTimer();
     }
-    handelChange=(e)=>{
-        this.setState({seconds:e.target.value});
+    handelChange=(e,type)=>{
+        e.persist();  //wont clear the object when we close the app
+       // console.log('handel change',e,type);
+        if(type=='hours'){
+            const secs = Number(e.target.value*60*60)+Number(this.state.m*60)+Number(this.state.s);    
+            this.setState({h:e.target.value,seconds:secs});
+        } //
+        if(type=='mins'){
+            const secs = Number(this.state.h*60*60)+Number(e.target.value*60)+Number(this.state.s);    
+            this.setState({m:e.target.value,seconds:secs});
+        }
+        if(type=='secs'){
+            const secs = Number(this.state.h*60*60)+Number(this.state.m*60)+Number(e.target.value);    
+            this.setState({s:e.target.value,seconds:secs});
+        }
+        
     }
     startset=()=>{
         const secs = Number(this.state.h*60*60)+Number(this.state.m*60)+Number(this.state.s);
         this.setState({starton:true,seconds:secs,stopon:false});
     }
+    resetTimer=()=>{
+        this.setState({h:0,m:0,s:0,seconds:0,timer:true,interval:0,stopon:false,starton:true,submiton:true});
+    }
 
     render(){
+        //flex boxes
         const stop = this.state.stopon?(
             <div>
                 <button onClick={this.stopTimer}>Stop</button>
@@ -62,29 +83,37 @@ class App extends Component{
                 <button onClick={this.changeTimer}>Start</button>
             </div>
         ):null;
-        const submit = this.state.submiton?(
+        const reset = (
             <div>
-                <button type="Submit" onClick={this.startset} >Submit</button>
+                <button onClick={this.resetTimer}>Reset</button>
             </div>
-        ):null;
+        );
         return(
-            <div> 
+            <div>
                 <div>
-                    <p>Enter Hours</p>
-                    <input type="number" onChange={e=>this.setState({h:e.target.value,submiton:true})} min="0"  />
-                    <p>Enter Minutes</p>
-                    <input type="number" onChange={e=>this.setState({m:e.target.value,submiton:true})} min="0" max="60" />
-                    <p>Enter Seconds</p>
-                    <input type="number" onChange={e=>this.setState({s:e.target.value,submiton:true})} min="0" max="60" />
+                    <h3>Countdown Timer</h3>
+                </div>
+                <div className="timelabel">
+                   <label htmlFor="hour">Hour</label>
+                    
+                    <label htmlFor="minute" className="labelmin">Minute</label>
+                    <label htmlFor="second">Second</label>
+                </div>
+                <div className="time">
+                    
+                    <input type="number" onChange={e=>this.handelChange(e,'hours')} min="0" value={this.state.h} id="hour" />
+                    <h4>:</h4>
+                    <input type="number" onChange={e=>this.handelChange(e,'mins')} min="0" max="60" value={this.state.m} id="minute" />
+                    <h4>:</h4>
+                    <input type="number" onChange={e=>this.handelChange(e,'secs')} min="0" max="60" value={this.state.s} id="second" />
                 </div>
                 <br></br>
-                {submit}
-                {start}
-                {stop}
-                <br></br>h:{this.state.h}
-                <br></br>m:{this.state.m}
-                <br></br>s:{this.state.s}
-                <br></br>
+                <div className="button">
+                    {start}
+                    {stop}
+                    {reset}          
+                </div>
+                
                 
             </div>
         )
